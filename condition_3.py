02a6488213ada9d6b384eb7fc7a8af2f200d8a47
Anugrah-Chandran <anugrahchandran2028@cs.sjcetpalai.ac.in>
2024-10-25 14:40:21 +0530

    Add files via upload

---
+++ condition_3.py
@@ -0,0 +1,11 @@
+'''Author: Anugrah Chandran
+Date: 25-10-24
+Description: Python program to find a number is positive,negative or a zero
+'''
+number=int(input("Enter a number:"))
+if number>0:
+    print(number," is a positive number")
+elif number<0:
+    print(number," is a negattive number")
+else:
+    print(number," is a zero")
